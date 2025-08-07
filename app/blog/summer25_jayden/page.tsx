b48469e977b0104d5908b246af8b4c7f6f64beb4
Jayden <jkohhokj@gmail.com>
2025-08-07 14:08:23 -0500

    add summer 25 jayden blog draft embedded iframe to notion

---
+++ app/blog/summer25_jayden/page.tsx
@@ -0,0 +1,16 @@
+import { title } from "@/components/primitives";
+
+export default function BlogPage() {
+  return (
+    <div className="min-w-5xl">
+      <h1 className={title()}>Summer 2025 - Jayden: RSD & CVA6</h1>
+
+      <iframe
+        src="https://billowy-noise-5f3.notion.site/ebd/248998e87ac580b1a88de5f248088f7d"
+        width="100%"
+        height="600"
+        // width="1000"
+        />
+    </div>
+  );
+}
