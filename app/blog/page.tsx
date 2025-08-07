3a4952ca4aee8b78517400e1f0de63b75fdcb973
Jayden <jkohhokj@gmail.com>
2025-08-07 14:14:19 -0500

    update blog post in blogs page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,22 @@
 import { title } from "@/components/primitives";
-
+import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
+import {Link} from "@heroui/link";
 export default function BlogPage() {
   return (
     <div>
       <h1 className={title()}>Blog</h1>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Card>
+        <CardHeader>
+          Integrating RSD & Superscalar CVA6
+        </CardHeader>
+        <CardBody>
+          <Link href="/blog/summer25_jayden">Link</Link>
+        </CardBody>
+      </Card>
     </div>
   );
 }
